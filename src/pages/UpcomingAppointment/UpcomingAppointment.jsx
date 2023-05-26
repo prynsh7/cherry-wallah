dfcc65b8da836e38d58a9f6b3812fe417ca847c4
AnandD17 <avdoddamani123@gmail.com>
2023-05-26 22:25:04 +0530

    patient appointment page

--- src/pages/UpcomingAppointment/UpcomingAppointment.jsx
+++ src/pages/UpcomingAppointment/UpcomingAppointment.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import DoctorProfile from '../../assets/images/doctorlist.png'
 import CustomPagination from '../../components/Pagination/Pagination'
 import { Pagination } from 'antd'
@@ -6,12 +6,30 @@ import Modal from '../../components/Modal/Modal'
 import { useNavigate } from 'react-router-dom'
 import { ROUTES } from '../../routes/RouterConfig'
 import TimeSlots from '../../components/TimeSlots/TimeSlots'
+import { AppointmentAPI } from '../../apis/appointmentAPI'
+import avatar from '../../assets/images/avatars/avatar.avif'
 
 const UpcomingAppointment = () => {
     const navigate = useNavigate()
     const [isOpen, setIsOpen] = useState(false)
     const handleCloseModal = () => setIsOpen(false)
     const handleOpenModal = () => setIsOpen(true)
+
+    const [data, setData] = useState([])
+
+    const getData = async() => {
+        try {
+            const res =await AppointmentAPI.getAppointments()
+            console.log(data)
+            if(res.success) setData(res.data.appointments)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(()=>{
+        getData()
+    },[])
     return (
 
         <div className='bg-neutral-4 pb-[60px]'>
@@ -37,9 +55,9 @@ const UpcomingAppointment = () => {
                 </div>
 
                 <div className='grid sm:grid-cols-3 gap-6 px-[34px] '>
-                    <div className=' gap-6 p-[40px] grid-col-1 bg-[#ffff] text-Medium+/Title/xSmall flex flex-col justify-center items-center'><span>Upcoming Appointments</span> <span className='text-[#FF9900] text-Medium+/Title/Large'>2</span></div>
-                    <div className=' gap-6 p-[40px] grid-col-1 bg-[#ffff] text-Medium+/Title/xSmall flex flex-col justify-center items-center'><span>Completed Appointments</span><span className='text-primary-6 text-Medium+/Title/Large'>17</span></div>
-                    <div className=' gap-6 p-[40px] grid-col-1 bg-[#ffff] text-Medium+/Title/xSmall flex flex-col justify-center items-center'><span>Cancelled Appointments</span><span className='text-[#FF0000] text-Medium+/Title/Large'>1</span></div>
+                    <div className=' gap-6 p-[40px] grid-col-1 bg-[#ffff] text-Medium+/Title/xSmall flex flex-col justify-center items-center'><span>Upcoming Appointments</span> <span className='text-[#FF9900] text-Medium+/Title/Large'>0</span></div>
+                    <div className=' gap-6 p-[40px] grid-col-1 bg-[#ffff] text-Medium+/Title/xSmall flex flex-col justify-center items-center'><span>Completed Appointments</span><span className='text-primary-6 text-Medium+/Title/Large'>{data?.length}</span></div>
+                    <div className=' gap-6 p-[40px] grid-col-1 bg-[#ffff] text-Medium+/Title/xSmall flex flex-col justify-center items-center'><span>Cancelled Appointments</span><span className='text-[#FF0000] text-Medium+/Title/Large'>0</span></div>
                 </div>
 
                 <div className='flex px-[34px] pt-[30px] gap-6 text-Medium+/Paragraph/Medium-Strong'>
@@ -48,17 +66,17 @@ const UpcomingAppointment = () => {
                     <button>Cancelled</button>
                 </div>
 
-                {[...Array(8)].map((item, index) => {
+                {data.map((item, index) => {
                     return <div className='bg-[#ffff] flex p-[24px] mx-[32px] mt-[18px] rounded-[10px] justify-between items-center'>
                         <div className='flex justify-center items-center gap-6 '>
                             <div className='text-center'>
-                                <img className='pb-[6px]' src={DoctorProfile} alt="" />
+                                <img className='pb-[6px] max-w-[200px] aspect-square rounded-full' src={item?.doctor?.profile_image || avatar} alt="" />
                             </div>
                             <div>
-                                <h3 className='text-Small/Title/Medium text-[#007E85]'>Dr. Priyansh Raj</h3>
-                                <p className='text-Small/Title/xSmall text-neutral-8'>Skin Specialist</p>
+                                <h3 className='text-Small/Title/Medium text-[#007E85]'>{item?.doctor?.name}</h3>
+                                <p className='text-Small/Title/xSmall text-neutral-8'>{item?.doctor?.profile_name}</p>
                                 <div className='border-2 p-[4px] rounded mt-2'>
-                                    <span>17th May 2023 - 9:30 PM</span>
+                                    <span>{item?.appointment_date} - {item?.appointment_time}</span>
                                 </div>
                             </div>
 
